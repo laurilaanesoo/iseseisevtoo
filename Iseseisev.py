98ad49ec46f22978eb135a0e7b66380fa7f63e6f
laurilaanesoo <98745890+laurilaanesoo@users.noreply.github.com>
2022-03-02 18:39:33 +0200

    Iseseisev

--- Iseseisev.py
+++ Iseseisev.py
@@ -2,29 +2,21 @@
 #02.03.2022
 #Lauri Laanesoo
 
+#Tuleb sisestada sõna, mida teha salakeeleks
+salakeel = input("Sisesta sõna, mida tahad teha salakeeleks: ")
+#Kasutab for funktsiooni, et tekitada suvalisi tähti
+for i in salakeel:
+    kiri = (chr(ord(i) + 5))
+    print(kiri,end='')
+#Väljastab sisestatud sõnumi salakeeles
+print(f"\nSisestatud Sõnum::{salakeel}")
 
+#Tõlgib soovitud sõna 
+tolkimine = input("Sisesta sõna, mida tahad tõlkida: " )
+tolkimine = tolkimine.lower().replace(" ", "")
+for i in tolkimine:
+    kiri = (chr(ord(i) - 5))
+    print(kiri,end='')
+#Väljastab sisestatud sõnumi tavatähtedega
+print(f"\nSisestatud sõnum: {tolkimine}")
 
-sisend = int(input("Kas soovid salakeelt luua või tõlkida? [1,2]:"))
-
-if sisend == 1:
-    salakeel = input("Sisesta sõna, mida tahad teha salakeeleks: ")
-    encMessage = rsa.encrypt(message.encode(),publicKey)
-    print(encMessage)
-else:
-    tolkimine = input("Sisesta sõna, mida tahad tõlkida: " )
-    decMessage = rsa.decrypt(encMessage, privateKey).decode()
-    
-    tahestik="abcdefghijklmnopqrstuvwxyz"
-sonum = input("Sisesta sõna, mida tahad krüpteerida")
-krupt,dekrupt="",""
-voti = 5
-
-for taht in sonum:
-    uus = (tahestik.find(taht)+voti)%len(tahestik)
-    krupt+=tahestik[uus]
-    
-for taht in krupt:
-    uus = (tahestik.find(taht)-voti)%len(tahestik)
-    dekrupt+=tahestik[uus]
-    
-print("Sõnum:",krupt)
